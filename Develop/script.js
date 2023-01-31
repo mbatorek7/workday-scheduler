07f8f1c303d743e4eb2f48f0ed2860bcb9f78b58
mbatorek7 <maegan.batorek@valpo.edu>
2023-01-30 21:01:25 -0800

    display current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +17,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let todaysDate = `${month}-${day}-${year}`;
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = todaysDate;
 });
